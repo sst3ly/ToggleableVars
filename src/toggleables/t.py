f9623f84c17d0875183268fadcd8104c951aad1d
sst3ly <69128003+sst3ly@users.noreply.github.com>
2021-08-15 11:46:36 -0700

    Delete src/toggleables directory

--- src/toggleables/t.py
+++
@@ -1,8 +0,0 @@
-class Toggleable():
-    def __init__(self, setto=False):
-        self.value = setto
-    def toggle(self):
-        if(self.value == False):
-            self.value = True
-        elif(self.value == True):
-            self.value = False
